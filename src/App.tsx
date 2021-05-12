9d47ca40d21cfffcfcbc870c0686a4ef31aecb1b
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-05-12 21:24:45 +0700

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const people = [
+  {
+    name: 'Calvin Hawkins',
+    email: 'calvin.hawkins@example.com',
+    image:
+      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Kristen Ramos',
+    email: 'kristen.ramos@example.com',
+    image:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Ted Fox',
+    email: 'ted.fox@example.com',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container min-w-full ">
+      <div className="btn">test</div>
+
+      <ul className="divide-y divide-gray-200">
+        {people.map((person) => (
+          <li key={person.email} className="py-4 flex">
+            <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+            <div className="ml-3">
+              <p className="text-sm font-medium text-gray-900">{person.name}</p>
+              <p className="text-sm text-gray-500">{person.email}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
