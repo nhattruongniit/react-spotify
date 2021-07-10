3a496c3dd96119157dac794ed95a03decc852f0b
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2021-07-10 22:34:43 +0700

    code logic volumn

--- src/components/molecules/Player/Player.tsx
+++ src/components/molecules/Player/Player.tsx
@@ -1,14 +1,47 @@
-
+import { useState } from 'react'
 import ProgressBar from './ProgressBar'
 
+import clsx from 'clsx'
+
 const Player = () => {
+  const [isPlay, seIsPlay] = useState(false);
+  
+  function togglePlay() {
+    seIsPlay(prevState => !prevState)
+  }
+
   return (
-    <div className="bg-player-bg fixed w-full bottom-0 left-0 h-24 px-4 flex justify-between items-center">
+    <div className="bg-player-bg fixed w-full bottom-0 left-0 h-24 px-10 flex justify-between items-center">
       <div className="w-1/5 flex items-center">
-        <div>
-          <div className="w-16 h-16 border-2 border-solid rounded-full overflow-hidden">
+        <div 
+          className={clsx(
+            'w-16 h-16 relative',
+            isPlay && 'music_played'
+          )}
+        >
+          <div className="thumbnail rounded-full overflow-hidden border-2 border-solid">
             <img src="assets/images/icon-album.jpeg" alt="Album" />
           </div>
+          <svg viewBox="0 0 512 512" className="musicNote note-1">
+            <path
+              d="M470.38 1.51L150.41 96A32 32 0 0 0 128 126.51v261.41A139 139 0 0 0 96 384c-53 0-96 28.66-96 64s43 64 96 64 96-28.66 96-64V214.32l256-75v184.61a138.4 138.4 0 0 0-32-3.93c-53 0-96 28.66-96 64s43 64 96 64 96-28.65 96-64V32a32 32 0 0 0-41.62-30.49z"
+            />
+          </svg>
+          <svg viewBox="0 0 384 512" className="musicNote note-2">
+            <path
+              d="M310.94 1.33l-96.53 28.51A32 32 0 0 0 192 60.34V360a148.76 148.76 0 0 0-48-8c-61.86 0-112 35.82-112 80s50.14 80 112 80 112-35.82 112-80V148.15l73-21.39a32 32 0 0 0 23-30.71V32a32 32 0 0 0-41.06-30.67z"
+            />
+          </svg>
+          <svg viewBox="0 0 512 512" className="musicNote note-3">
+            <path
+              d="M470.38 1.51L150.41 96A32 32 0 0 0 128 126.51v261.41A139 139 0 0 0 96 384c-53 0-96 28.66-96 64s43 64 96 64 96-28.66 96-64V214.32l256-75v184.61a138.4 138.4 0 0 0-32-3.93c-53 0-96 28.66-96 64s43 64 96 64 96-28.65 96-64V32a32 32 0 0 0-41.62-30.49z"
+            />
+          </svg>
+          <svg viewBox="0 0 512 512" className="musicNote note-4">
+            <path
+              d="M470.38 1.51L150.41 96A32 32 0 0 0 128 126.51v261.41A139 139 0 0 0 96 384c-53 0-96 28.66-96 64s43 64 96 64 96-28.66 96-64V214.32l256-75v184.61a138.4 138.4 0 0 0-32-3.93c-53 0-96 28.66-96 64s43 64 96 64 96-28.65 96-64V32a32 32 0 0 0-41.62-30.49z"
+            />
+          </svg>
         </div>
         <div className="w-44 ml-3 overflow-hidden whitespace-nowrap overflow-ellipsis">
           If You Really Love Me (How Will I Know)
@@ -22,8 +55,13 @@ const Player = () => {
           <button type="button">
             <i className="icon icon--xsmall flex ic-pre mr-3" />
           </button>
-          <button type="button">
-            <i className="icon icon--4xlarge flex ic-play-circle-outline mr-3" />
+          <button type="button" onClick={togglePlay}>
+            <i 
+              className={clsx(
+                "icon icon--4xlarge flex mr-3",
+                isPlay ? 'ic-pause-circle-outline' : 'ic-play-circle-outline'
+              )}
+            />
           </button>
           <button type="button">
             <i className="icon icon--xsmall flex ic-next mr-3" />
--- src/components/molecules/Player/ProgressBar.tsx
+++ src/components/molecules/Player/ProgressBar.tsx
@@ -1,9 +1,60 @@
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 
 function ProgressBar() {
+  // States
+  const [percent, setPercent] = useState(50);
+  // Refs
+  const progressRef = useRef<HTMLDivElement>(null)
+  const isBusyRef = useRef(false)
+
+  function handleVolume(event: { type: string; nativeEvent: { offsetX: number; }; }) {
+    if(!progressRef.current) return;
+    if(event.type === 'click') {
+      const newValue = (event.nativeEvent.offsetX /  progressRef.current.offsetWidth) * 100;
+      setPercent(newValue)
+    }
+    if(event.type === 'mousedown') {
+      isBusyRef.current = true
+    }
+  }
+
+  const hanldeMouseMove = useCallback((event: any) => {
+    if(!progressRef.current) return;
+    if(isBusyRef.current) {
+      const offsetWidth = progressRef.current.offsetWidth;
+      let cal = event.clientX - progressRef.current.offsetLeft;
+      if(cal < 0) cal = 0;
+      if(cal > offsetWidth) cal = offsetWidth;
+
+      const newValue = (cal / offsetWidth) * 100;
+      setPercent(newValue)
+    }
+  },[])
+
+  function hanldeMouseUp() {
+    isBusyRef.current = false
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousemove', hanldeMouseMove)
+    document.addEventListener('mouseup', hanldeMouseUp)
+
+    return () => {
+      document.removeEventListener('mousemove', hanldeMouseMove)
+      document.removeEventListener('mouseup', hanldeMouseUp)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <div className="progressBar">
+    <div
+      ref={progressRef}
+      className="progressBar" 
+      onClick={handleVolume}
+      onMouseDown={handleVolume}
+      >
       <div className="progressBar__bg">
-        <div className="progressBar__dot" style={{ width: '70%' }} />
+        <div className="progressBar__dot" style={{ width: `${Math.round(percent)}%` }} />
       </div>
     </div>
   )
