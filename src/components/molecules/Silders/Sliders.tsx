46f3a15a8701ba60d5791143b2f0c0167c29c8a9
TonyNguyen <truong.nn@amanotes.com>
2022-06-20 16:38:10 +0700

    update Slider

--- src/components/molecules/Silders/Sliders.tsx
+++ src/components/molecules/Silders/Sliders.tsx
@@ -1,20 +1,27 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback } from "react";
 
 // data
-import sliders from './data.json';
+import sliders from "./data.json";
 
 // components
-import SliderItem from './SliderItem';
+import SliderItem from "./SliderItem";
 
 export default function Sliders() {
   const [currIndex, setCurrIndex] = useState(Math.floor(Math.random() * 6));
 
+  console.log("no squash commit");
+
   const handlePrevSlide = useCallback(() => {
-    setCurrIndex((prevCurrIndex: number) => (prevCurrIndex - 1 + sliders.length) % sliders.length)
+    setCurrIndex(
+      (prevCurrIndex: number) =>
+        (prevCurrIndex - 1 + sliders.length) % sliders.length
+    );
   }, []);
 
   const handleNextSlide = useCallback(() => {
-    setCurrIndex((prevCurrIndex: number) => (prevCurrIndex + 1) % sliders.length)
+    setCurrIndex(
+      (prevCurrIndex: number) => (prevCurrIndex + 1) % sliders.length
+    );
   }, []);
 
   // useEffect(() => {
@@ -26,21 +33,31 @@ export default function Sliders() {
     <div className="sliders">
       <div className="sliders__container">
         <div className="sliders__prev" onClick={handlePrevSlide}>
-          <button type="button" className="sliders__control sliders__control--prev"><i className="icon ic-go-left" /></button>
+          <button
+            type="button"
+            className="sliders__control sliders__control--prev"
+          >
+            <i className="icon ic-go-left" />
+          </button>
         </div>
         {sliders.map((slide, index) => (
-          <SliderItem 
-            key={slide.id} 
+          <SliderItem
+            key={slide.id}
             index={index}
             slide={slide}
-            currIndex={currIndex} 
+            currIndex={currIndex}
             total={sliders.length}
           />
         ))}
         <div className="sliders__next" onClick={handleNextSlide}>
-          <button type="button" className="sliders__control sliders__control--next"><i className="icon ic-go-right" /></button>
+          <button
+            type="button"
+            className="sliders__control sliders__control--next"
+          >
+            <i className="icon ic-go-right" />
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
