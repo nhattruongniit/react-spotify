39724ed5c3ef6873eeb9e9def10ef570fca17edf
TonyNguyen <truong.nn@amanotes.com>
2022-06-20 16:37:57 +0700

    update SliderItem

--- src/components/molecules/Silders/SliderItem.tsx
+++ src/components/molecules/Silders/SliderItem.tsx
@@ -1,41 +1,44 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
 type IProps = {
   slide: {
-    id: number,
-    banner: string
-  },
+    id: number;
+    banner: string;
+  };
   currIndex: number;
   index: number;
   total: number;
-}
+};
 
 function SliderItem({ slide, currIndex, index, total }: IProps) {
   let next = currIndex + 1;
   let prev = currIndex - 1;
 
-  if(currIndex === total - 1) {
+  if (currIndex === total - 1) {
     next = 0;
-  } else if(currIndex === 0) {
+  } else if (currIndex === 0) {
     prev = 4;
   }
 
+  console.log("no squash commit");
+
   return (
     <React.Fragment>
-      <div 
+      <div
         className={clsx(
-          'sliders__item',
-          index === prev && 'sliders__item--previous',
-          index === currIndex && 'sliders__item--selected',
-          index === next && 'sliders__item--next',
-        )}>
+          "sliders__item",
+          index === prev && "sliders__item--previous",
+          index === currIndex && "sliders__item--selected",
+          index === next && "sliders__item--next"
+        )}
+      >
         <figure>
           <img src={slide.banner} alt="Banner" />
         </figure>
       </div>
     </React.Fragment>
-  )
+  );
 }
 
-export default SliderItem
+export default SliderItem;
